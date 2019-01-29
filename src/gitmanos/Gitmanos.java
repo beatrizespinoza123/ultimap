bcf69fd95f38484f465288c4cab994a0c6cf938d
GIOAND <andresmaldonadogit123@gmail.com>
2019-01-29 12:12:35 -0600

    contribui al proyecto

---
+++ src/gitmanos/Gitmanos.java
@@ -0,0 +1,30 @@
+
+package gitmanos;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Gitmanos {
+
+    
+    public static void main (String [] args)
+   {
+      InputStreamReader isr = new InputStreamReader(System.in);
+      BufferedReader br = new BufferedReader (isr);
+      try
+      {
+         System.out.print("Sumando 1 : ");
+         int s1 = Integer.parseInt(br.readLine());
+         System.out.print("Sumando 2 : ");
+         int s2 = Integer.parseInt(br.readLine());
+         int sumacion=s1+s2;
+         System.out.println ("La suma es " + s1 + "+" + s2 +"="+ sumacion);
+      }
+      catch (IOException | NumberFormatException e)
+      {
+      }
+    
+   }
+    
+}
